5bb453a3ed2aadaf174c4898971ff03949fce0a9
Luan Monteiro <luan.monteiro@spacesheepgames.com>
2022-07-19 18:57:57 -0300

    Fixed requests

--- scripts/consultas.sql
+++ scripts/consultas.sql
@@ -18,30 +18,15 @@ from objetos_arte oa
 full join emprestados e on e.numObj4 = oa.numId
 full join permanentes pe on pe.numObj5 = oa.numId;
 
+
 -- 2. liste as colecoes com o maior numero de emprestimos por mes e por ano (maior quantidade de objetos emprestados?)
-SELECT MAX(sub.count), sub.mes
-from (select COUNT(e.nomecolpert) as count, e.nomecolpert, EXTRACT(month from e.dataemprestimo) as num_mes,
-CASE
-	WHEN EXTRACT(month from e.dataemprestimo) = 1 THEN 'JANEIRO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 2 THEN 'FEVEREIRO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 3 THEN 'MARÇO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 4 THEN 'ABRIL'
-	WHEN EXTRACT(month from e.dataemprestimo) = 5 THEN 'MAIO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 6 THEN 'JUNHO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 7 THEN 'JULHO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 8 THEN 'AGOSTO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 9 THEN 'SETEMBRO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 10 THEN 'OUTUBRO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 11 THEN 'NOVEMBRO'
-	WHEN EXTRACT(month from e.dataemprestimo) = 12 THEN 'DEZEMBRO'
-END as mes
-from emprestados e
-group by num_mes, nomecolpert
-order by num_mes) as sub
-group by mes
+select count(e.numObj4) as qtd_total_objetos, co.nomecol from colecao co
+join emprestados e on e.nomecolpert = co.nomecol
+group by co.nomecol
+order by qtd_total_objetos desc;
 
 -- 3. listagem da compra de objetos de arte por mes e por ano
-select CAST(SUM(pe.custo) as money) as custo_total, EXTRACT(month from pe.dataaquisicao) as num_mes,
+select CAST(SUM(pe.custo) as money) as custo_total, EXTRACT(month from pe.dataaquisicao) as numMes,
 CASE
 	WHEN EXTRACT(month from pe.dataaquisicao) = 1 THEN 'JANEIRO'
 	WHEN EXTRACT(month from pe.dataaquisicao) = 2 THEN 'FEVEREIRO'
@@ -58,8 +43,8 @@ CASE
 END as mes,
 EXTRACT(year from pe.dataaquisicao) as ano
 from permanentes pe
-group by num_mes, ano 
-order by num_mes, ano;
+group by numMes, ano
+order by numMes, ano;
 
 -- simplificada skdksdks
 select CAST(SUM(pe.custo) as money) as custo_total, EXTRACT(month from pe.dataaquisicao) as mes,
@@ -76,3 +61,9 @@ from colecao co
 join emprestados e on e.nomeColPert = co.nomeCol
 group by co.nomeCol, mes, ano
 order by mes, ano;
+
+-- CONSULTAS COMPLEMENTARES-----------------------------------
+select *
+from permanentes pe
+WHERE EXTRACT(month from pe.dataaquisicao) = 5 and
+EXTRACT(year from pe.dataaquisicao) = 1977;
